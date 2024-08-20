85fdaf2a9e930ede4d815c4a2fa25dc8c64ec5f3
Satyam <satyamagrawalread@gmail.com>
2024-08-21 02:27:19 +0530

    add env for backend url

--- src/constant.ts
+++ src/constant.ts
@@ -1,7 +1,7 @@
-export const AVATAR_API = "https://ui-avatars.com/api";
-export const API = "http://localhost:1337/api";
-// export const API = "https://chat-app-backend-alpha-sandy.vercel.app/api";
+// export const AVATAR_API = "https://ui-avatars.com/api";
+// export const API = "http://localhost:1337/api";
+export const API = `${import.meta.env.VITE_APP_BACKEND_URL}/api`;
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
-// export const URL = "https://chat-app-backend-alpha-sandy.vercel.app";
-export const URL = "http://localhost:1337";
+export const URL = import.meta.env.VITE_APP_BACKEND_URL;
+// export const URL = "http://localhost:1337";
